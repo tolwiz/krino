f257a8dac1306185848b81bb89030ea833bc7924
tolwiz <metacirculardispatches@gmail.com>
2025-08-05 13:14:25 +0200

    Implemented mat_rand and mat_fill

--- kr.h
+++ kr.h
@@ -2,6 +2,7 @@
 #define NN_H_
 
 #include <stddef.h>
+#include <stdio.h>
 
 #ifndef NN_MALLOC
 #include <stdlib.h>
@@ -31,7 +32,12 @@ typedef struct {
 } Mat;
 
 #define MAT_AT(m, i, j) (m).es[(i)*(m).cols + (j)]
+
+float rand_float(void);
+
 Mat mat_alloc(size_t rows, size_t cols);
+void mat_fill(Mat m, float x);
+void mat_rand(Mat m, float low, float high);
 void mat_dot(Mat dst, Mat a, Mat b);
 void mat_sum(Mat dst, Mat a);
 void mat_print(Mat m);
@@ -39,6 +45,10 @@ void mat_print(Mat m);
 #endif // NN_H_
 
 #ifdef NN_IMPLEMENTATION
+
+float rand_float(void) {
+    return (float) rand() / (float) RAND_MAX;
+}
 
 Mat mat_alloc(size_t rows, size_t cols){
     Mat m;
@@ -56,12 +66,38 @@ void mat_dot(Mat dst, Mat a, Mat b) {
 }
 
 void mat_sum(Mat dst, Mat a){
-    (void) dst;
-    (void) a;
+    NN_ASSERT(dst.rows == a.rows);
+    NN_ASSERT(dst.cols == a.cols);
+    for (size_t i = 0; i < dst.rows; ++i) {
+        for (size_t j = 0; j < dst.rows; ++j) {
+            MAT_AT(dst, i, j) += MAT_AT(a, i, j);
+        }
+    }
 }
 
 void mat_print(Mat m) {
-    (void) m;
+    for (size_t i = 0; i < m.rows; ++i) {
+        for (size_t j = 0; j < m.cols; ++j) {
+            printf("%f ", MAT_AT(m, i, j));
+        }
+        printf("\n");
+    }
+}
+
+void mat_fill(Mat m, float x) {
+    for (size_t i = 0; i < m.rows; ++i) {
+        for (size_t j = 0; j < m.cols; ++j) {
+            MAT_AT(m, i, j) = x; 
+        }
+    }
+}
+
+void mat_rand(Mat m, float low, float high) {
+    for (size_t i = 0; i < m.rows; ++i) {
+        for (size_t j = 0; j < m.cols; ++j) {
+            MAT_AT(m, i, j) = rand_float()*(high - low) + low;
+        }
+    }
 }
 
 #endif // NN_IMPLEMENTATION
--- nn.c
+++ nn.c
@@ -1,9 +1,21 @@
+#include <time.h>
+
 #define NN_IMPLEMENTATION
 #include "kr.h"
 
 int main(void) {
-    Mat m = mat_alloc(2, 2);
-    mat_print(m);
+    srand(time(0));
+
+    Mat a = mat_alloc(2, 2);
+    mat_fill(a, 1);
+
+    Mat b = mat_alloc(2, 2);
+    mat_fill(a, 1);
+    
+    mat_print(a);
+    printf("--------------------------------------\n");
+    mat_sum(a, b);
+    mat_print(a);
 
     return 0;
 }
