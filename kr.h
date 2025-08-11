e4781ad306f9da52f44f46410dee1eef5a3c30f8
tolwiz <metacirculardispatches@gmail.com>
2025-08-11 16:39:36 +0200

    Get rid of useless particular code and keep general abstractions

--- kr.h
+++ kr.h
@@ -49,8 +49,8 @@ void mat_copy(Mat dst, Mat src);
 void mat_dot(Mat dst, Mat a, Mat b);
 void mat_sum(Mat dst, Mat a);
 void mat_sig(Mat m);
-void mat_print(Mat m, const char *name);
-#define MAT_PRINT(m) mat_print(m, #m)
+void mat_print(Mat m, const char *name, size_t padding);
+#define MAT_PRINT(m) mat_print(m, #m, 0)
 
 typedef struct {
     size_t count;
@@ -59,10 +59,17 @@ typedef struct {
     Mat *as; // The amount of activations is count+1
 } NN;
 
+#define NN_INPUT(nn) (nn).as[0]
+#define NN_OUTPUT(nn) (nn).as[(nn).count]
+
 NN nn_alloc(size_t *arch, size_t arch_count); 
 void nn_print(NN nn, const char *name);
 #define NN_PRINT(nn) nn_print(nn, #nn);
-
+void nn_rand(NN nn, float low, float high);
+void nn_forward(NN nn);
+float nn_cost(NN nn, Mat ti, Mat to);
+void nn_finite_diff(NN nn, NN g, float eps, Mat ti, Mat to);
+void nn_learn(NN nn, NN g, float rate);
 
 #endif // NN_H_
 
@@ -91,7 +98,8 @@ void mat_dot(Mat dst, Mat a, Mat b) {
     size_t n = a.cols;
     NN_ASSERT(dst.rows == a.rows);
     NN_ASSERT(dst.cols == b.cols);
-
+    mat_fill(dst, 0.0f);
+    
     for (size_t i = 0; i < dst.rows; ++i) {
         for (size_t j = 0; j < dst.cols; ++j) {
             for (size_t k = 0; k < n; ++k) {
@@ -138,15 +146,16 @@ void mat_sig(Mat m) {
     }
 }
 
-void mat_print(Mat m, const char *name) {
-    printf("%s = [\n", name);
+void mat_print(Mat m, const char *name, size_t padding) {
+    printf("%*s%s = [\n", (int) padding, "", name);
     for (size_t i = 0; i < m.rows; ++i) {
+        printf("%*s    ", (int) padding, "");
         for (size_t j = 0; j < m.cols; ++j) {
-            printf("    %f ", MAT_AT(m, i, j));
+            printf("%f ", MAT_AT(m, i, j));
         }
         printf("\n");
     }
-    printf("]\n");
+    printf("%*s]\n", (int) padding, "");
 }
 
 void mat_fill(Mat m, float x) {
@@ -179,7 +188,7 @@ NN nn_alloc(size_t *arch, size_t arch_count) {
     NN_ASSERT(nn.as != NULL);
     
     nn.as[0] = mat_alloc(1, arch[0]);
-    for (size_t i = 1; i > arch_count; ++i) {
+    for (size_t i = 1; i < arch_count; ++i) {
         nn.ws[i-1] = mat_alloc(nn.as[i-1].cols, arch[i]);
         nn.bs[i-1] = mat_alloc(1, arch[i]);
         nn.as[i]   = mat_alloc(1, arch[i]);
@@ -189,12 +198,91 @@ NN nn_alloc(size_t *arch, size_t arch_count) {
 }
 
 void nn_print(NN nn, const char *name) {
+    char buf[256];
     printf("%s = [\n", name);
     for (size_t i = 0; i < nn.count; ++i) {
-        MAT_PRINT(nn.ws[i]);
-        MAT_PRINT(nn.bs[i]);
+        snprintf(buf, sizeof(buf), "ws%zu", i);
+        mat_print(nn.ws[i], "ws", 4);
+        snprintf(buf, sizeof(buf), "bs%zu", i);
+        mat_print(nn.bs[i], "bs", 4);
     }
     printf("]\n");
+}
+
+void nn_rand(NN nn, float low, float high) {
+    for (size_t i = 0; i < nn.count; ++i) {
+        mat_rand(nn.ws[i], low, high);
+        mat_rand(nn.bs[i], low, high);
+    }
+}
+
+float nn_cost(NN nn, Mat ti, Mat to) {
+    assert(ti.rows == to.rows);
+    assert(to.cols == NN_OUTPUT(nn).cols);
+    size_t n = ti.rows;
+
+    float c = 0;
+    for (size_t i = 0; i < n; ++i) {
+        Mat x = mat_row(ti, i);
+        Mat y = mat_row(to, i);
+
+        mat_copy(NN_INPUT(nn), x);
+        nn_forward(nn);
+        size_t q = to.cols;
+        for (size_t j = 0; j < q; ++j) {
+            float d = MAT_AT(NN_OUTPUT(nn), 0, j) - MAT_AT(y, 0, j);
+            c += d*d;
+        }
+    }
+
+    return c/n;
+}
+
+void nn_finite_diff(NN nn, NN g, float eps, Mat ti, Mat to) {
+    float saved;
+    float c = nn_cost(nn, ti, to);
+
+    for (size_t i = 0; i < nn.count; ++i) {
+        for (size_t j = 0; j < nn.ws[i].rows; ++j) {
+            for (size_t k = 0; k < nn.ws[i].cols; ++k) {
+                saved = MAT_AT(nn.ws[i], j, k);
+                MAT_AT(nn.ws[i], j, k) += eps;
+                MAT_AT(g.ws[i], j, k) = (nn_cost(nn, ti, to) - c)/eps;
+                MAT_AT(nn.ws[i], j, k) = saved;
+            }
+        }
+        for (size_t j = 0; j < nn.bs[i].rows; ++j) {
+            for (size_t k = 0; k < nn.bs[i].cols; ++k) {
+                saved = MAT_AT(nn.bs[i], j, k);
+                MAT_AT(nn.bs[i], j, k) += eps;
+                MAT_AT(g.bs[i], j, k) = (nn_cost(nn, ti, to) - c)/eps;
+                MAT_AT(nn.bs[i], j, k) = saved;
+            }
+        }
+    }
+}
+
+void nn_forward(NN nn) {
+    for (size_t i = 0; i < nn.count; ++i) {
+        mat_dot(nn.as[i+1], nn.as[i], nn.ws[i]);
+        mat_sum(nn.as[i+1], nn.bs[i]);
+        mat_sig(nn.as[i+1]);
+    }
+}
+
+void nn_learn(NN nn, NN g, float rate) {
+    for (size_t i = 0; i < nn.count; ++i) {
+        for (size_t j = 0; j < nn.ws[i].rows; ++j) {
+            for (size_t k = 0; k < nn.ws[i].cols; ++k) {
+                MAT_AT(nn.ws[i], j, k) -= rate*MAT_AT(g.ws[i], j, k);
+            }
+        }
+        for (size_t j = 0; j < nn.bs[i].rows; ++j) {
+            for (size_t k = 0; k < nn.bs[i].cols; ++k) {
+                MAT_AT(nn.bs[i], j, k) -= rate*MAT_AT(g.bs[i], j, k);
+            }
+        }
+    }
 }
 
 #endif // NN_IMPLEMENTATION
--- nn.c
+++ nn.c
@@ -3,132 +3,6 @@
 #define NN_IMPLEMENTATION
 #include "kr.h"
 
-typedef struct {
-    //input
-    Mat a0;
-    //first layer
-    Mat w1, b1, a1;
-    //second layer
-    Mat w2, b2, a2;
-} Xor;
-
-Xor xor_alloc(void) {
-    Xor m;
-    m.a0 = mat_alloc(1, 2);
-    m.w1 = mat_alloc(2, 2);
-    m.b1 = mat_alloc(1, 2);
-    m.a1 = mat_alloc(1, 2);
-    m.w2 = mat_alloc(2, 1);
-    m.b2 = mat_alloc(1, 1);
-    m.a2 = mat_alloc(1, 1);
-    return m;
-}
-
-float forward_xor(Xor m) {
-    
-    // pass input through the first layer
-    mat_dot(m.a1, m.a0, m.w1);
-    mat_sum(m.a1, m.b1);
-    mat_sig(m.a1);
-
-    // pass input through the second layer
-    mat_dot(m.a2, m.a1, m.w2);
-    mat_sum(m.a2, m.b2);
-    mat_sig(m.a2);
-    
-    return *m.a2.es;
-}
-
-float cost(Xor m, Mat ti, Mat to) {
-    assert(ti.rows == to.rows);
-    assert(to.cols == m.a2.cols);
-    size_t n = ti.rows;
-
-    float c = 0;
-    for (size_t i = 0; i < n; ++i) {
-        Mat x = mat_row(ti, i);
-        Mat y = mat_row(to, i);
-
-        mat_copy(m.a0, x);
-        forward_xor(m);
-
-        size_t q = to.cols;
-        for (size_t j = 0; j < q; ++j) {
-            float d = MAT_AT(m.a2, 0, j) - MAT_AT(y, 0, j);
-            c += d*d;
-        }
-    }
-
-    return c/n;
-}
-
-void finite_diff(Xor m, Xor g, float eps, Mat ti, Mat to) {
-    float saved;
-    float c = cost(m, ti, to);
-
-    for (size_t i = 0; i < m.w1.rows; ++i) {
-        for (size_t j = 0; j < m.w1.cols; ++j) {
-            saved = MAT_AT(m.w1, i, j);
-            MAT_AT(m.w1, i, j) += eps;
-            MAT_AT(g.w1, i, j) = (cost(m, ti, to) -c)/eps;
-            MAT_AT(m.w1, i, j) = saved;
-        }
-    }
-    
-    for (size_t i = 0; i < m.b1.rows; ++i) {
-        for (size_t j = 0; j < m.b1.cols; ++j) {
-            saved = MAT_AT(m.b1, i, j);
-            MAT_AT(m.b1, i, j) += eps;
-            MAT_AT(g.b1, i, j) = (cost(m, ti, to) -c)/eps;
-            MAT_AT(m.b1, i, j) = saved;
-        }
-    }
-    
-    for (size_t i = 0; i < m.w2.rows; ++i) {
-        for (size_t j = 0; j < m.w2.cols; ++j) {
-            saved = MAT_AT(m.w2, i, j);
-            MAT_AT(m.w2, i, j) += eps;
-            MAT_AT(g.w2, i, j) = (cost(m, ti, to) -c)/eps;
-            MAT_AT(m.w2, i, j) = saved;
-        }
-    }
-    
-    for (size_t i = 0; i < m.b2.rows; ++i) {
-        for (size_t j = 0; j < m.b2.cols; ++j) {
-            saved = MAT_AT(m.b2, i, j);
-            MAT_AT(m.b2, i, j) += eps;
-            MAT_AT(g.b2, i, j) = (cost(m, ti, to) -c)/eps;
-            MAT_AT(m.b2, i, j) = saved;
-        }
-    }
-}
-
-void xor_learn(Xor m, Xor g, float rate) {
-    for (size_t i = 0; i < m.w1.rows; ++i) {
-        for (size_t j = 0; j < m.w1.cols; ++j) {
-            MAT_AT(m.w1, i, j) -= rate*MAT_AT(g.w1, i, j);
-        }
-    }
-    
-    for (size_t i = 0; i < m.b1.rows; ++i) {
-        for (size_t j = 0; j < m.b1.cols; ++j) {
-            MAT_AT(m.b1, i, j) -= rate*MAT_AT(g.b1, i, j);
-        }
-    }
-    
-    for (size_t i = 0; i < m.w2.rows; ++i) {
-        for (size_t j = 0; j < m.w2.cols; ++j) {
-            MAT_AT(m.w2, i, j) -= rate*MAT_AT(g.w2, i, j);
-        }
-    }
-    
-    for (size_t i = 0; i < m.b2.rows; ++i) {
-        for (size_t j = 0; j < m.b2.cols; ++j) {
-            MAT_AT(m.b2, i, j) -= rate*MAT_AT(g.b2, i, j);
-        }
-    }
-}
-
 float td[] = {
    0, 0, 0,
    0, 1, 1,
@@ -138,14 +12,8 @@ float td[] = {
 
 int main(void) {
     
-    size_t arch[] = {2, 2, 1};
-    NN nn = nn_alloc(arch, ARRAY_LEN(arch));
-    NN_PRINT(nn);
-
-    return 0;
-
     srand(time(0));
-
+    
     size_t stride = 3;
     size_t n = sizeof(td)/sizeof(td[0])/stride;
     Mat ti = {
@@ -162,37 +30,30 @@ int main(void) {
         .es = td + 2
     };
 
-    Xor m = xor_alloc();
-    Xor g = xor_alloc();
-
-    mat_rand(m.w1, 0, 1);
-    mat_rand(m.b1, 0, 1);
-    mat_rand(m.w2, 0, 1);
-    mat_rand(m.b2, 0, 1);
+    size_t arch[] = {2, 2, 1};
+    NN nn = nn_alloc(arch, ARRAY_LEN(arch));
+    NN g = nn_alloc(arch, ARRAY_LEN(arch));
+    nn_rand(nn, 0, 1);
 
     float eps = 1e-1;
     float rate = 1e-1;
 
-    printf("cost = %f\n", cost(m, ti, to));
-    for (size_t i = 0; i < 1000*1000; ++i) {
-        finite_diff(m, g, eps, ti, to);
-        xor_learn(m, g, rate);
-        printf("%zu: cost = %f\n", i, cost(m, ti, to));
+    printf("cost = %f\n", nn_cost(nn, ti, to));
+    
+    for (size_t i = 0; i < 20*1000; ++i) {
+        nn_finite_diff(nn, g, eps, ti, to);
+        nn_learn(nn, g, rate);
+        printf("%zu: cost = %f\n", i, nn_cost(nn, ti, to));
     }
-   
-    printf("---------------------\n");
-#if 1
+
     for (size_t i = 0; i < 2; ++i) {
         for (size_t j = 0; j < 2; ++j) {
-            MAT_AT(m.a0, 0, 0) = i;
-            MAT_AT(m.a0, 0, 1) = j;
-            forward_xor(m);
-            float y = *m.a2.es;
-
-            printf("%zu ^ %zu = %f\n", i, j, y);
+            MAT_AT(NN_INPUT(nn), 0, 0) = i;
+            MAT_AT(NN_INPUT(nn), 0, 1) = j;
+            nn_forward(nn);
+            printf("%zu ^ %zu = %f\n", i, j, MAT_AT(NN_OUTPUT(nn), 0, 0));
         }
     }
-#endif
 
     return 0;
 }
